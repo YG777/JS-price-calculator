59b61baafe8af49d7f9d22440197ff973739653e
YG777 <katyake@gamil.com>
2018-03-02 16:22:51 +0000

    first commit

---
+++ calculator.js
@@ -0,0 +1,22 @@
+var data = {
+  'a5Postcard': {
+    'a5BasePrice': 40,
+    'a5BasePriceColour': 50
+  },
+  'a6Postcard': {
+    'a6BasePrice': 39,
+    'a6BasePriceColour': 47
+  },
+  'greetingCard': {
+    'basePrice': 100
+  },
+  'letter': {
+    'basePrice': 44,
+    'basePriceColour': 49,
+    'extraSheet': 4,
+    'extraSheetColour': 6,
+    'duplexExtra': 2,
+    'duplexExtraColour': 3,
+    'windowedEnvelopeExtra': 1
+  }
+};
