925194b044bf2287e64a15a09fa48b54811647e1
YG777 <katyake@gamil.com>
2018-03-02 18:08:59 +0000

    created logic

--- calculator.js
+++ calculator.js
@@ -20,3 +20,37 @@ var data = {
     'windowedEnvelopeExtra': 1
   }
 };
+ 
+//DUMMY ORDER
+var cardOrderA5 = 6;
+var cardOrderA6 = 10;
+var letterOrder = {
+  'bw': 16,
+  'color': 15,
+  'extraSheet': 10,
+  'extraSheetColour': 5,
+  'duplexExtra': 3,
+  'duplexExtraColour': 6,
+  'windowedEnvelopeExtra': 15
+};
+  
+const letterPrices = Object.values(data['letter']); 
+const letterOrderCosts = Object.values(letterOrder);
+const lettersTotalPrice = () =>{
+  let lettersSum = 0;
+  for(let i=0; i< letterPrices.length; i++) {
+    lettersSum += letterPrices[i]*letterOrderCosts[i];
+  }
+  return lettersSum;
+};
+  
+const cardOrderA5TotalPrice = Object.values(data['a5Postcard'])[1]*cardOrderA5; 
+const cardOrderA6TotalPrice = Object.values(data['a6Postcard'])[1]*cardOrderA6;
+  
+const orderTotal = () => {
+  const totalSum = (lettersTotalPrice() + cardOrderA6TotalPrice + cardOrderA5TotalPrice)/100;
+  return totalSum;
+};
+  
+orderTotal();
+  
