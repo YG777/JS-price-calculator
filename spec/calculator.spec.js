37c6fd122cc15f4d3a6b9a366b0e490ccc1513c4
YG777 <katyake@gamil.com>
2018-03-06 12:31:20 +0000

    calculate function

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -1,22 +1,23 @@
 var calculator = require('../src/calculator');
+
 describe('Calculator', function() {
-
-  const order = {
-    type: 'letter',
-    quantity: 25,
-    options: {
-      colour: true,
-      duplexColour: true,
-      extraSheetColour: 2,
-      windowed: true
-    }
-  };
-
-  it('calculate 1letter price ', function () {
-
-    const options = order.options; 
-    expect(calculator.letterColourPrices(options)).toBe(65);  
-  });
+  describe('letter pricing', function () {
+    it('calculate 1 black letter', function () {
+      expect(calculator.calculate({quantity: 1, type: 'letter'})).toBe(44);  
+    });
   
- 
+    it('calculate 10 black letters', function(){
+      expect(calculator.calculate({ quantity: 10, type: 'letter'})).toBe(440);
+    });
+  });
+
+  describe('postcard pricing', function(){
+    it('calculate 1 A5 black postcard', function(){
+      expect(calculator.calculate({quantity: 1, type: 'a5postcard'})).toBe(39);
+    });
+
+    it('calculate 10 A5 black postcard', function(){
+      expect(calculator.calculate({quantity: 10, type: 'a5postcard'})).toBe(390);
+    });
+  });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -1,100 +1,21 @@
-// //DUMMY PRICE DATA
-var data = {
-  a5Postcard: {
-    a5BasePrice: 40,
-    a5BasePriceColour: 50
-  },
-  a6Postcard: {
-    a6BasePrice: 39,
-    a6BasePriceColour: 47
-  },
-  greetingCard: {
-    basePrice: 100
-  },
-  letter: {
-    basePriceColour: 49,
-    extraSheetColour: 6,
-    duplexExtraColour: 3,
-    windowedEnvelopeExtra: 1
-  }
-};
 
-const order = {
-  type: 'letter',
-  quantity: 25,
-  options: {
-    colour: true,
-    duplexColour: true,
-    extraSheetColour: 2,
-    windowed: true
-  }
-};
-//ORDER VARS
-const type                        = order.type;
-const quantity                    = order.quantity;
-const options                     = order.options; 
-//ORDER OBJ OPTIONS VARS
-const orderNumLetterXsheetcolour  = order.options.extraSheetColour;
-const orderLetterDuplexColour     = order.options.duplexColour;
-const orderLetterWindowed         = order.options.windowed;
-const orderNumLetterXsheet        = order.options.extraSheet;
-const orderLetterDuplex           = order.options.duplex;
-//PRICE VARS
-const priceLetterColour           = data.letter.basePriceColour;
-const priceLetterXcolour          = data.letter.extraSheetColour;
-const priceLetterDuplexColour     = data.letter.duplexExtraColour;
-const priceLetterWind             = data.letter.windowedEnvelopeExtra;
-const priceLetter                 = data.letter.basePrice;
-const priceLetterX                = data.letter.extraSheet;
 
-const getType = (type) => {
-  if (type === 'letter') {
-    letterOptions(options);
-  } else if (type === 'a5Postcard' || type === 'a6Postcard') {
-    postcardOptions(options);
-  }else if (type === 'greetingCard'){
-    greetingcardOptions(options);
-  }
-};
-
-const letterOptions = (options) => {
-  if (options.colour) {
-    console.log('-option colour, calling lettercolourPrices');
-    letterColourPrices(options);
-  }else {
-    letterBasePrices(options);
-  }
-};
-
-const letterColourPrices  = (options) => {
-  let retVal = 0;
-  Object.keys(options).forEach(k => { 
-    if (k === 'colour' && options[k]) {
-      retVal = retVal + priceLetterColour;
-      // console.log(retVal);
-    } else if (k === 'duplexColour' && options[k]) {
-      retVal = retVal + priceLetterDuplexColour;
-      // console.log(retVal);
-    } else if (k === 'extraSheetColour' && options[k]) {
-      retVal = retVal + (orderNumLetterXsheetcolour*priceLetterXcolour);
-      // console.log(retVal);
-    }
-    else if (k === 'windowed' && options[k]) {
-      retVal = retVal + orderLetterWindowed;
-      // console.log(retVal);
-    }
-  });
-  return retVal;
-};
+function calculate(options){ //options = {quantity: 1, type: 'letter'}
+  var price = 0;
+if (options.type === 'letter') {
+    price = 44;
   
+  } else if (options.type === 'a5postcard') {
+    price = 39;
+   
+  }
 
+  return price * options.quantity;
 
-
+}
 
 
 
 module.exports = {
-  getType,
-  letterOptions,
-  letterColourPrices
+  calculate  
 };
