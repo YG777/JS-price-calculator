1086cd8a94f66ae89a11d3432a026424d6b39001
YG777 <katyake@gamil.com>
2018-03-06 13:05:03 +0000

    tests and code for color

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -8,13 +8,28 @@ describe('Calculator', function() {
 
     it('calculate 1 black letter', function () {
       options.quantity = 1;
+      options.colour = false;
       expect(calculator.calculate(options)).toBe(44);  
     });
   
     it('calculate 10 black letters', function(){
       options.quantity = 10;
+      options.colour = false;
       expect(calculator.calculate(options)).toBe(440);
     });
+
+    it('calculate 1 colour letter', function () {
+      options.quantity = 1;
+      options.colour = true;
+      expect(calculator.calculate(options)).toBe(49);  
+    });
+  
+    it('calculate 10 colour letters', function(){
+      options.quantity = 10;
+      options.colour = true;
+      expect(calculator.calculate(options)).toBe(490);
+    });
+
   });
 
   describe('postcard pricing', function(){
@@ -24,12 +39,28 @@ describe('Calculator', function() {
 
     it('calculate 1 A5 black postcard', function(){
       options.quantity = 1;
-      expect(calculator.calculate(options)).toBe(39);
+      options.colour = false;
+      expect(calculator.calculate(options)).toBe(40);
     });
 
     it('calculate 10 A5 black postcard', function(){
       options.quantity = 10;
-      expect(calculator.calculate(options)).toBe(390);
+      options.colour = false;
+      expect(calculator.calculate(options)).toBe(400);
+    });    
+    
+    it('calculate 1 A5 colour postcard', function(){
+      options.quantity = 1;
+      options.colour = true;
+      expect(calculator.calculate(options)).toBe(50);
     });
+
+    it('calculate 10 A5 colour postcard', function(){
+      options.quantity = 10;
+      options.colour = true;
+      expect(calculator.calculate(options)).toBe(500);
+    });
+
+    
   });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -1,19 +1,18 @@
-
-
-function calculate(options){ //options = {quantity: 1, type: 'letter'}
+function calculate(options) {
+  //options = {quantity: 1, type: 'letter'}
   var price = 0;
-  if (options.type === 'letter') {
+  if (options.type === "letter" && !options.colour) {
     price = 44;
-  } else if (options.type === 'a5postcard') {
-    price = 39;
-   
+  } else if (options.type === "letter" && options.colour) {
+    price = 49;
+  } else if (options.type === "a5postcard" && !options.colour) {
+    price = 40;
+  } else if (options.type === "a5postcard" && options.colour) {
+    price = 50;
   }
   return price * options.quantity;
-
 }
 
-
-
 module.exports = {
-  calculate  
+  calculate
 };
