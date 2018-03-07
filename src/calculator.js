f26c5d6a213514d2147ab3b90f8ff6e9a339afbf
YG777 <katyake@gamil.com>
2018-03-07 18:47:09 +0000

    refactored

--- src/calculator.js
+++ src/calculator.js
@@ -2,23 +2,33 @@ function Calculator(prices) {
   this.prices = prices;
 
   this.calculate = function (options) {
-    let price = 0;
-    if (options.type === 'letter' && !options.colour) {
-      price = this.prices.letter.basePrice;
-    } else if (options.type === 'letter') {
-      price = this.prices.letter.colourPrice;
-    } else if (options.type === 'a5postcard' && !options.colour) {
-      price = this.prices.a5postcard.basePrice;
-    } else if (options.type === 'a5postcard') {
-      price = this.prices.a5postcard.colourPrice;
-    } else if (options.type === 'a6postcard' && !options.colour) {
-      price = this.prices.a6postcard.basePrice;
-    }else if (options.type === 'a6postcard') {
-      price = this.prices.a6postcard.colourPrice;
-    }else if (options.type === 'greetingCard') {
-      price = this.prices.greetingCard.basePrice;
-    }
-    return price * options.quantity;
+    const type = options.type;
+    const prices = this.prices;
+    const colour = options.colour;
+    const calculateBase = () => {
+      let price = 0;
+      if (type === 'letter' && !colour) {
+        price = prices.letter.basePrice;
+      } else if (type === 'letter') {
+        price = prices.letter.colourPrice;
+      } else if (type === 'a5postcard' && !colour) {
+        price = prices.a5postcard.basePrice;
+      } else if (type === 'a5postcard') {
+        price = prices.a5postcard.colourPrice;
+      } else if (type === 'a6postcard' && !colour) {
+        price = prices.a6postcard.basePrice;
+      }else if (type === 'a6postcard') {
+        price = prices.a6postcard.colourPrice;
+      }else if (type === 'greetingCard') {
+        price = prices.greetingCard.basePrice;
+      }
+      return price * options.quantity;
+    };
+    return calculateBase();
+
+  // const calculateLetterXras = () => {
+  //   if (type === 'letter' &&)
+  // }
   };
 }
 
