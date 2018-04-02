163375bcfd97b99e7871644065a57b0c9813c6de
YG777 <katyake@gamil.com>
2018-04-03 00:39:16 +0100

    ui calculator - getting values into an obj

--- src/calculatorUI.js
+++ src/calculatorUI.js
@@ -1,11 +1,44 @@
+function readValuesAndCreateObject() {
+  console.log(
+    'capture all the values into the correct object and log to console'
+  );
+  var quantity = $('#quantity').val();
+  var extraSheets = $('#extraSheets').val();
+
+  var isColour = $('input[name="colour"]:checked').val();
+  var isDuplex = $('input[name="duplex"]:checked').val();
+  var isWindowedEnvelope = $('input[name="envelope"]:checked').val();
+
+  const options = {
+    quantity,
+    extraSheets,
+    colour: isColour,
+    duplex: isDuplex,
+    windowedEnvelopeExtra: isWindowedEnvelope
+  };
+  console.log(options);
+}
+
+
 $(function() {
-  $(".input-range").on("input", function() {
-    $(this)
-      .next(".range-value")
-      .html(this.value);
+  //slider
+  $('#quantity').on('change', function() {
+    const slider = $(this);
+    const qty = slider.val();
+    slider.next('.range-value').html(qty);
+    readValuesAndCreateObject();
   });
+
+  $('#extraSheets').on('change', function() {
+    const slider = $(this);
+    const qty = slider.val();
+    slider.next('.range-value').html(qty);
+    readValuesAndCreateObject();
+  });
+
+  $('input[type="radio"]').on('change', function() {
+    readValuesAndCreateObject();
+  });
+
 });
-
-
-
 
